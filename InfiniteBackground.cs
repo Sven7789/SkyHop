f8c0a3f495e5f061ef0b010c9120907e89da302c
Sven Nieuwenhuizen <62598817+Sven7789@users.noreply.github.com>
2024-06-04 20:38:09 +0200

    Oneindige achtergrond toegevoegd

---
+++ InfiniteBackground.cs
@@ -0,0 +1,24 @@
+/*
+ Autheur: Sven Nieuwenhuizen
+ Onderwerp: InfiniteBackground Script
+ Datum: 27-05-2024
+ */
+using UnityEngine;
+
+public class InfiniteBackground : MonoBehaviour
+{
+    public Transform cameraTransform; // De referentie naar de camera
+    private Vector3 initialOffset;
+
+    void Start()
+    {
+        // Bereken de initiële offset tussen de achtergrond en de camera
+        initialOffset = transform.position - cameraTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        // Update de positie van de achtergrond om de offset te behouden
+        transform.position = cameraTransform.position + initialOffset;
+    }
+}
