cdf6cf931bc4f9ecf35757151a62e02e7272fba5
Sven Nieuwenhuizen <62598817+Sven7789@users.noreply.github.com>
2024-06-04 15:20:41 +0200

    Knoppen toegevoegd aan Game Over

--- GameOverManager.cs
+++ GameOverManager.cs
@@ -4,20 +4,27 @@
  Datum: 27-05-2024
  */
 using UnityEngine;
+using UnityEngine.UI; // Voor Button
 
 public class GameOverManager : MonoBehaviour
 {
     public GameObject GameOver; // Verwijzing naar het Game Over Canvas
+    public Button BackToMain; // Verwijzing naar de BackToMain knop
+    public Button Retry; // Verwijzing naar de Retry knop
 
     void Start()
     {
-        // Zorg ervoor dat het Game Over Canvas in het begin uitgeschakeld is
+        // Zorg ervoor dat het Game Over Canvas en de knoppen in het begin uitgeschakeld zijn
         GameOver.SetActive(false);
+        BackToMain.gameObject.SetActive(false);
+        Retry.gameObject.SetActive(false);
     }
 
     public void ShowGameOverScreen()
     {
-        // Activeer het Game Over Canvas
+        // Activeer het Game Over Canvas en de knoppen
         GameOver.SetActive(true);
+        BackToMain.gameObject.SetActive(true);
+        Retry.gameObject.SetActive(true);
     }
 }
